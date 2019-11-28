09e5ab4d63c98945a09bf8e02ce812122f7d312a
Suriya <suriya.sadaiyappan@solitontech.com>
2019-11-28 12:46:31 +0530

    Update App layout and style
    Signed-off-by: Suriya Prashath <suriyaprashath@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import React from 'react';
-import './App.css';
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch
-} from 'react-router-dom';
-import PhaseView from './components/PhaseView/PhaseView';
+import React from "react";
+import "./App.scss";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route path="/">
-          <PhaseView></PhaseView>
-        </Route>
-      </Switch>
-    </Router>
-  );
+import Header from "./components/Header/Header";
+import Sidebar from "./components/Sidebar/Sidebar";
+import PhaseView from "./components/PhaseView/PhaseView";
+
+export class App extends React.Component {
+  render() {
+    return (
+      <>
+        <Header></Header>
+        <div className="page-body">
+          <div className="sidebar-ctr">
+            <Sidebar></Sidebar>
+          </div>
+          <div className="route-content">
+            <Router>
+              <Switch>
+                <Route path="/" component={PhaseView} />
+                <Route path="/phaseview" component={PhaseView} />
+              </Switch>
+            </Router>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
--- src/components/PhaseView/PhaseView.js
+++ src/components/PhaseView/PhaseView.js
@@ -3,11 +3,6 @@ import Header from "../Header/Header";
 
 export default class PhaseView extends React.Component {
   render() {
-    return (
-      <>
-        <Header></Header>
-        
-      </>
-    );
+    return <></>;
   }
 }
