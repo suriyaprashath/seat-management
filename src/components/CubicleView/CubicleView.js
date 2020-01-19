e0609327c642f0ff14f5d6a591358b06b3ed8991
Suriya Prashath <suriyaprashath@gmail.com>
2020-01-19 20:15:15 +0530

    Update cubicle view to include user edit, unlink and view cubicle elements
    Functionalities for the UI elements are not added.
    
    Signed-off-by: Suriya Prashath <suriyaprashath@gmail.com>

--- src/components/CubicleView/CubicleView.js
+++ src/components/CubicleView/CubicleView.js
@@ -14,6 +14,7 @@ class CubicleView extends React.Component {
               <span className="seat">Seat</span>
               <span className="name">Name</span>
               <span className="team">Team</span>
+              <span className="unlink">Unlink</span>
             </div>
             <div className="cubicle-detail-bdy">
               {this.props.seatData.selectedPhase.cubicles.map(
@@ -25,7 +26,17 @@ class CubicleView extends React.Component {
                       }`}
                       key={cubicle.id}
                     >
-                      <span className="cubicle">{cubicle.name}</span>
+                      <div className="cubicle">
+                        <span className="cubicle-name">{cubicle.name}</span>
+                        <div className="view-ctr">
+                          View
+                          <img
+                            className="view-icon"
+                            src="./assets/images/pencil.png"
+                            alt="Go"
+                          />
+                        </div>
+                      </div>
                       <div className="seat-details-ctr">
                         {cubicle.seats.map((seat, seatIndex) => {
                           return (
@@ -36,6 +47,26 @@ class CubicleView extends React.Component {
                               key={seat.id}
                             >
                               <span className="seat">{seat.name}</span>
+                              <div className="name">
+                                <span>
+                                  {seat.occupant && seat.occupant.name
+                                    ? seat.occupant.name
+                                    : "-"}
+                                </span>
+                                <img
+                                  className="edit-occupant"
+                                  src="./assets/images/pencil.png"
+                                  alt="Edit"
+                                />
+                              </div>
+                              <span className="team"></span>
+                              <div className="unlink">
+                                <img
+                                  className="unlink-icon"
+                                  src="./assets/images/unlink.png"
+                                  alt="Unlink"
+                                />
+                              </div>
                             </div>
                           );
                         })}
