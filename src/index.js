4f358e0da84e6ff143aeff4a698cb09c7fc9eaed
Suriya Prashath <suriyaprashath@gmail.com>
2020-01-24 11:21:16 +0530

    Add reset.css to the project
    Signed-off-by: Suriya Prashath <suriyaprashath@gmail.com>

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 
 import "./index.scss";
+import "./reset.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import configureStore from "./store";
